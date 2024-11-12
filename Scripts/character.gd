f0c788f131d12f4a0aa0cf72d220933344def980
cactys3 <139386859+Cactys3@users.noreply.github.com>
2024-11-12 02:56:19 -0500

    omg2

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var SPEED: int = 300
+@export var SPEED: int = 210
 var moving: bool = false
 @export var health_component:HealthComponent
 
@@ -10,18 +10,18 @@ func _physics_process(_delta: float) -> void:
 func handle_moving() -> void:
 	var directionX := Input.get_axis("left", "right")
 	if directionX:
-		velocity.x = directionX * SPEED
+		velocity.x = move_toward(velocity.x, directionX * SPEED, 65)#directionX * SPEED
 		moving = true
 	else:
 		moving = false
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 100)
 		
 	var directionY := Input.get_axis("up", "down")
 	if directionY:
-		velocity.y = directionY * SPEED
+		velocity.y = move_toward(velocity.y, directionY * SPEED, 65)#directionY * SPEED
 		moving = true
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, 100)
 	
 	if moving:
 		moving = false #TODO: walk animation
