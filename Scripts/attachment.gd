b8ad6656a6a35d7754cabd0cf53c5a789a797a21
cactys3 <139386859+Cactys3@users.noreply.github.com>
2025-01-29 17:10:46 -0500

    More on sword_attachment

--- Scripts/attachment.gd
+++ Scripts/attachment.gd
@@ -12,26 +12,30 @@ var bullets: Array[Projectile]
 @export var handle: Handle
 @export var projectile: PackedScene
 #new stuff
-@export var continuous_hitbox: bool = false
 var cooldown_timer: float = 0
 var attacking: bool = false
 
 func _ready() -> void:
 	cooldown_timer = 0
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	process_cooldown(delta)
+
+## Should handle cooldown and calling attack()
+## this is meant to be overridden by classes that inherit it
+func process_cooldown(delta: float) -> void: 
 	if attacking:
 		pass
 	elif cooldown_timer <= frame.get_stat(stats.COOLDOWN):
-		cooldown_timer += _delta
+		cooldown_timer += delta
 		print(cooldown_timer)
 	elif handle.ready_to_fire:
 		attacking = true
 		attack()
 
-
-
-func attack(): #this is meant to be overridden by classes that inherit it
+## Should handle creating a projectile, shooting it, and reseting cooldown
+## this is meant to be overridden by classes that inherit it
+func attack(): 
 	#projectile = preload("res://Scenes/luger_bullet.tscn")
 	#print(projectile.resource_name)
 	var new_bullet:Projectile = projectile.instantiate()
@@ -41,3 +45,22 @@ func attack(): #this is meant to be overridden by classes that inherit it
 	get_tree().root.add_child(new_bullet)
 	cooldown_timer = 0
 	attacking = false
+
+func init_projectile(new_position: Vector2, new_scale: Vector2, new_direction: Vector2) -> Projectile:
+	if projectile == null:
+		push_error("projectile null in attachment script")
+		return null
+	var new_bullet:Projectile = projectile.instantiate()
+	new_bullet.global_position = new_position
+	new_bullet.scale = new_scale
+	new_bullet.setup(frame, new_direction)
+	get_tree().root.add_child(new_bullet)#TODO: maybe better options exist
+	return new_bullet
+
+
+#how should attachment extenders work?
+#Create methods for ProcessCooldown(delta) and Attack() that extenders override.
+#Create generic methods for creating a projectile with the right size/parent so extenders can use that
+#extenders should always (with exceptions) call super on read and process 
+#extenders have their own hitbox references and do all that themselves
+#extenders have their own animations but have to include handle in these animations? or animations work to change position of weapon_frame?
--- Scripts/sword_scripts/sword_attachment.gd
+++ Scripts/sword_scripts/sword_attachment.gd
@@ -1,15 +1,30 @@
 extends Attachment
 
+@export var continous_hitbox: CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	super()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
+
+func _process(delta: float) -> void:
+	process_cooldown(delta)
+
+func process_cooldown(delta: float) -> void:
+	if attacking:
+		pass
+	elif cooldown_timer <= frame.get_stat(stats.COOLDOWN):
+		cooldown_timer += delta
+		print(cooldown_timer)
+	elif handle.ready_to_fire:
+		attacking = true
+		attack()
+
+func attack() -> void:
+	var new_projectile: Projectile = init_projectile(global_position, frame.scale, Vector2(cos(frame.rotation), sin(frame.rotation)))
+	#play animation
+	cooldown_timer = 0
+	attacking = false
 #how should attachment extenders work?
 #Create methods for ProcessCooldown(delta) and Attack() that extenders override.
 #Create generic methods for creating a projectile with the right size/parent so extenders can use that
