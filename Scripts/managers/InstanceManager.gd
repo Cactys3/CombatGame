c3ae75b8b2211a487305f96bca900f5f6cdfd73d
cactys3 <139386859+Cactys3@users.noreply.github.com>
2025-09-23 16:19:52 -0400

    it works, spawning randomly when generating a new places

--- Scripts/managers/InstanceManager.gd
+++ Scripts/managers/InstanceManager.gd
@@ -24,10 +24,9 @@ var spawning_cooldown: float
 @export var spawn_area: CollisionShape2D
 @export var spawn_deadzone: CollisionShape2D
 @export var background_parent: Node2D
+@export var event_parent: Node2D
 var spawning: bool = false
-var num_shops: int = 50
-var num_forges: int = 50
-var proximity_dic: Dictionary
+var event_arr: Array
 var map_height: int = 10 ## this many chunks tall
 var map_width: int = 10 ## this many chunks wide
 
@@ -55,18 +54,6 @@ func _ready() -> void:
 	spawning_stopwatch = 0 
 	spawning_cooldown = 1
 	chunk_rect = ColorRect.new()
-	for i in num_forges:
-		var new_forge: Vector2 = Vector2(randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2), randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2))
-		if !proximity_dic.has(new_forge):
-			proximity_dic.get_or_add(new_forge, [FORGE, false])
-		else:
-			proximity_dic.get_or_add(Vector2(randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2), randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2)), FORGE)
-	for i in num_shops:
-		var new_shop: Vector2 = Vector2(randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2), randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2))
-		if !proximity_dic.has(new_shop):
-			proximity_dic.get_or_add(new_shop, [SHOP, false])
-		else:
-			proximity_dic.get_or_add(Vector2(randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2), randi_range(-(map_width * chunk_x) / 2, (map_height * chunk_y) / 2)), SHOP)
 
 func _process(delta: float) -> void:
 	handle_chunks()
@@ -108,23 +95,7 @@ func handle_chunks():
 		refresh = false
 	if refresh:
 		draw_new_visual()
-		print("refresh:")
-		var spawnables: Array[PackedScene]
-		
-		for key in proximity_dic:
-			#print("/n/nlooking at key: " + str(key))
-			if Rect2(chunk_grid * Vector2(-640*1.5, -360*1.5), Vector2(640 * 3, 360 * 3)).has_point(key):
-				#print("point has key: " + str(key))
-				var thing: Array = proximity_dic.get(key)
-				if thing[1] == false:
-					#print("key is being instantiated at: " + str(key))
-					proximity_dic.set(key, [thing[0], true])
-					var real_thing = thing[0].instantiate()
-					GameManager.instance.xp_parent.add_child(real_thing)
-					real_thing.position = key
-					print("new key exists: " + str(real_thing.position))
-			else:
-				pass#print(str(Rect2(chunk_grid * Vector2(640, 360), Vector2(640 * 6, 360 * 6))) + " does not have point: " + str(key))
+		print("refresh: " + str(chunk_grid))
 		
 		if !chunks_dic.has(chunk_grid):
 			load_chunk(chunk_grid)
@@ -167,15 +138,39 @@ func load_chunk(chunk_id: Vector2):
 	var new_chunk: Sprite2D = Sprite2D.new()
 	new_chunk.visible = false
 	print(abs(chunk_id))
+	var generate_shop: int = 0
+	var generate_forge: int = 0
 	if abs(chunk_id.x) > abs(map_width) || abs(chunk_id.y) > abs(map_height):
 		new_chunk.texture = TileBlank
 	else:
 		new_chunk.texture = Tile1
+		while randf() > 0.95:
+			generate_forge += 1
+		while randf() > 0.85:
+			generate_shop += 1
+	while generate_shop > 0:
+		generate_shop -= 1
+		load_event(SHOP, chunk_id)
+	while generate_forge > 0:
+		generate_forge -= 1
+		load_event(FORGE, chunk_id)
 	background_parent.add_child(new_chunk)
 	new_chunk.position = ((chunk_id) * Vector2(640, 360))
 	chunks.append(new_chunk)
 	chunks_dic.get_or_add(chunk_id, new_chunk)
 	new_chunk.visible = true
+
+func load_event(scene: PackedScene, chunk: Vector2):
+	var new_event = scene.instantiate()
+	event_parent.add_child(new_event)
+	## Get random point inside the chunk and spawn event
+	var center := Vector2(chunk.x * chunk_x, chunk.y * chunk_y)
+	var half := Vector2(chunk_x, chunk_y) / 2.0
+	var new_min := center - half
+	var new_max := center + half
+	new_event.position = Vector2(randf_range(new_min.x, new_max.x), randf_range(new_min.y, new_max.y))
+	event_arr.append(new_event)
+	print("Load in Chunk: " + str(chunk) + " Event at: " + str(new_event.position))
 
 func draw_new_visual():
 	chunk_rect.color = Color(0, 0, 0, 0)
