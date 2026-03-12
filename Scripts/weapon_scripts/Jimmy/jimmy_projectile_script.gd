afc2093550bb02b242795bbe06ba9032e3fc1255
Sam <139386859+Cactys3@users.noreply.github.com>
2026-03-12 15:21:06 -0400

    Drew Jimmy Dragon Gun

---
+++ Scripts/weapon_scripts/Jimmy/jimmy_projectile_script.gd
@@ -0,0 +1,7 @@
+extends Projectile
+
+## On death, turn into lingering state  
+## Linger: pulse collision and apply burn every second
+## Don't damage enemies (maybe), just apply the burn
+
+## TODO: Implement
